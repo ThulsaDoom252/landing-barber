9a3cd979fdfa1126d13a5ba90987b29ff44d099c
ThulsaDoom252 <xenolm252@gmail.com>
2023-10-23 14:40:23 +0300

    Initial commit
    cleared react-app, assets added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'reset-css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+   <div>Barber shop</div>
+  );
+}
+
+export default App;
